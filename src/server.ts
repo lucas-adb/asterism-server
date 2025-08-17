d5f43b8946d5921bd2cfb79ed024bce69f0e2c26
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-08-17 15:20:09 -0300

    chore: sets vitest and biome

--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,5 @@ app
     host: '0.0.0.0', // to listen on all IPv4 addresses, not only localhost
     port: 3333,
   })
+  // biome-ignore lint/suspicious/noConsole: <initializing app>
   .then(() => console.log('Its Alive ⚡'));
