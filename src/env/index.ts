4ce2ba4e4e4871c1e0637c33415e7216dd718969
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-08-17 18:06:29 -0300

    chore: adds use case, repository, env and docker compose folder/files

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import z from 'zod';
+
+const defaultPortNumber = 3333;
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(defaultPortNumber),
+  JWT_SECRET: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  const prettyError = z.prettifyError(_env.error);
+  throw new Error(prettyError);
+}
+
+export const env = _env.data;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from './app';
+import { env } from './env';
 
 app
   .listen({
     host: '0.0.0.0', // to listen on all IPv4 addresses, not only localhost
-    port: 3333,
+    port: env.PORT,
   })
   // biome-ignore lint/suspicious/noConsole: <initializing app>
   .then(() => console.log('Its Alive ⚡'));
